c44b919b13c56a849be356ee6c2c7fc3c6ff52e6
Carl Schwan <carl@carlschwan.eu>
2020-10-01 12:02:30 +0200

    Many small improvements to the toc sidebar
    And update css version from CDN

--- assets/js/base.js
+++ assets/js/base.js
@@ -63,3 +63,97 @@ limitations under the License.
 
 
 }(jQuery));
+
+function getOffsetSum(elem) {
+    var top=0, left=0
+    while (elem) {
+        top = top + parseInt(elem.offsetTop)
+        left = left + parseInt(elem.offsetLeft)
+        elem = elem.offsetParent
+    }
+    return { top: top, left: left }
+}
+
+function setActiveMenuItem(li) {
+    var isMenuItem = li.matches('#TableOfContents > ul > li')
+    var isSubMenuItem = li.matches('#TableOfContents > ul > li > ul > li')
+    var wasActive = li.classList.contains('active')
+
+    if (isMenuItem || !wasActive) {
+        // Unset current active item
+        var menuItem = document.querySelector('#TableOfContents > ul > li.active')
+        if (menuItem) {
+            menuItem.classList.remove('active')
+        }
+        var menuItem = document.querySelector('#TableOfContents > ul > li > ul > li.active')
+        if (menuItem) {
+            menuItem.classList.remove('active')
+        }
+    }
+    li.classList.add('active')
+
+    if (isMenuItem) {
+        var firstSubMenuItem = li.querySelector('ul > li')
+        if (firstSubMenuItem) {
+            firstSubMenuItem.classList.add('active')
+        }
+    } else if (isSubMenuItem) {
+        var menuItem = li.parentNode.parentNode // TODO: parentUntil('li')
+        menuItem.classList.add('active')
+    }
+
+    // Scroll to item
+    // li.scrollIntoView()
+}
+function getMenuItemElement(li) {
+    var id = li.querySelector('a').href.split('#', 2)[1]
+    return document.getElementById(id)
+}
+function updateTOC() {
+    var viewTop = window.scrollY
+    var tocList = document.querySelectorAll('#TableOfContents li')
+    // Binary Search maybe (since elements are in order)?
+    for (var i = 0; i < tocList.length; i++) {
+        var curMenuItem = tocList[i]
+        var curElement = getMenuItemElement(curMenuItem)
+        var offset = getOffsetSum(curElement)
+        var offsetDiff = viewTop - offset.top
+        if (offsetDiff < 0) {
+            if (i <= 1) {
+                // At top, select the first item
+                setActiveMenuItem(curMenuItem)
+            } else {
+                var prevMenuItem = tocList[i - 1]
+                var prevElement = getMenuItemElement(prevMenuItem)
+                var prevOffset = getOffsetSum(prevElement)
+                var prevOffsetDiff = viewTop - prevOffset.top
+                var sectionReadRatio = (prevOffsetDiff)/prevMenuItem.offsetHeight
+                if (sectionReadRatio >= 0.7) {
+                    // Current item is being read
+                    setActiveMenuItem(curMenuItem)
+                } else {
+                    // Previous item is still being read
+                    setActiveMenuItem(prevMenuItem)
+                }
+            }
+            break
+        } else if (i == tocList.length - 1) {
+            // At bottom, select last item
+            setActiveMenuItem(curMenuItem)
+        }
+    }
+}
+
+var updating = false
+function queueUpdateTOC() {
+    if (!updating) {
+        updating = true
+        requestAnimationFrame(function() {
+            updateTOC()
+            updating = false
+        })
+    }
+}
+
+queueUpdateTOC()
+window.addEventListener('scroll', queueUpdateTOC)
