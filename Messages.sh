d07defc62932eafd3e69af5cd6521acfa1906293
Carl Schwan <carl@carlschwan.eu>
2021-06-05 14:01:33 +0200

    Enable extraction of strings

---
+++ Messages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2020 Carl Schwan <carl@carlschwan.eu>
+#
+# SPDX-LicenseIdentifier: BSD-2-Clause
+
+hugoi18n extract $podir
---
+++ scripts/fetch_po.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# SPDX-FileCopyrightText: 2021 Nguyen Hung Phu <phuhnguyen@disroot.org>
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+""" We only use this file's existence as a flag
+"""
