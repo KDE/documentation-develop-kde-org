a4175913427ce0b871a33fd5bc82aaffb2cf4a3b
Phu Nguyen <phu.nguyen@kdemail.net>
2022-05-14 06:31:31 +0200

    Remove fetch langs to fetch all, handle custom generation completely

--- scripts/custom_generation.py
+++ scripts/custom_generation.py
@@ -1,4 +1,7 @@
 import os
 
+os.environ["PACKAGE"] = 'documentation-develop-kde-org'
 os.system('git clone https://invent.kde.org/websites/hugo-i18n && pip3 install ./hugo-i18n')
+os.system('hugoi18n fetch pos')  # fetch translations into folder "pos"
+os.system('hugoi18n compile pos')  # compile translations in folder "pos"
 os.system('hugoi18n generate')
