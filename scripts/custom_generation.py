0631b0ececffd5a036b8cc6225061527c6e13997
Phu Nguyen <phu.nguyen@kdemail.net>
2022-05-29 00:44:38 +0200

    Download stuff in this script instead of in a separate step in build pipeline

--- scripts/custom_generation.py
+++ scripts/custom_generation.py
@@ -5,3 +5,5 @@ os.system('git clone https://invent.kde.org/websites/hugo-i18n && pip3 install .
 os.system('hugoi18n fetch pos')  # fetch translations into folder "pos"
 os.system('hugoi18n compile pos')  # compile translations in folder "pos"
 os.system('hugoi18n generate')
+
+os.system('python3 scripts/extract-plasma-applet-config-keys.py')
