48450d87a5ff3c00bf02b1aaea66acc26167691a
Phu Nguyen <phu.nguyen@kdemail.net>
2022-10-05 14:24:05 +0200

    Use po folder

--- scripts/custom_generation.py
+++ scripts/custom_generation.py
@@ -2,8 +2,7 @@ import os
 
 os.environ["PACKAGE"] = 'documentation-develop-kde-org'
 os.system('git clone https://invent.kde.org/websites/hugo-i18n && pip3 install ./hugo-i18n')
-os.system('hugoi18n fetch pos')  # fetch translations into folder "pos"
-os.system('hugoi18n compile pos')  # compile translations in folder "pos"
+os.system('hugoi18n compile po')  # compile translations in folder "po"
 os.system('hugoi18n generate')
 
 os.system('python3 scripts/extract-plasma-applet-config-keys.py')
