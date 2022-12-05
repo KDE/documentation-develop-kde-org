5142dab26f5d92f3f527eaa4d1dcf69c349e51ca
Thiago Sueto <herzenschein@gmail.com>
2022-12-05 16:55:30 -0300

    [mainwindow] reformat .cpp

--- content/docs/use/kxmlgui/main_window/main.cpp
+++ content/docs/use/kxmlgui/main_window/main.cpp
@@ -1,4 +1,3 @@
-#include <cstdlib>
 #include <QApplication>
 #include <QCommandLineParser>
 #include <KAboutData>
@@ -11,29 +10,20 @@ int main (int argc, char *argv[])
     KLocalizedString::setApplicationDomain("mainwindow");
     
     KAboutData aboutData(
-                         // The program name used internally. (componentName)
-                         QStringLiteral("mainwindow"),
-                         // A displayable program name string. (displayName)
-                         i18n("Main Window"),
-                         // The program version string. (version)
-                         QStringLiteral("1.0"),
-                         // Short description of what the app does. (shortDescription)
-                         i18n("A simple text area"),
-                         // The license this code is released under
-                         KAboutLicense::GPL,
-                         // Copyright Statement (copyrightStatement = QString())
-                         i18n("(c) 2015"),
-                         // Optional text shown in the About box.
-                         // Can contain any information desired. (otherText)
-                         i18n("Some text..."),
-                         // The program homepage string. (homePageAddress = QString())
-                         QStringLiteral("https://example.kde.org/"),
-                         // The bug report email address
-                         // (bugsEmailAddress = QLatin1String("submit@bugs.kde.org")
-                         QStringLiteral("submit@bugs.kde.org"));
+        QStringLiteral("mainwindow"),
+        i18n("Main Window"),
+        QStringLiteral("1.0"),
+        i18n("A simple text area"),
+        KAboutLicense::GPL,
+        i18n("(c) 2015"),
+        i18n("Some text..."),
+        QStringLiteral("https://example.kde.org/"),
+        QStringLiteral("submit@bugs.kde.org"));
 
-    aboutData.addAuthor(i18n("Name"), i18n("Task"), QStringLiteral("your@email.com"),
-                         QStringLiteral("http://your.website.com"), QStringLiteral("OSC Username"));
+    aboutData.addAuthor(i18n("Name"), i18n("Task"),
+        QStringLiteral("your@email.com"),
+        QStringLiteral("http://your.website.com"),
+        QStringLiteral("OSC Username"));
 
     KAboutData::setApplicationData(aboutData);
  
