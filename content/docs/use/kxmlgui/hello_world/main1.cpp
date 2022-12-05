d7395eb9a2801032b8cba6871fea4672b9c99d3f
Thiago Sueto <herzenschein@gmail.com>
2022-12-05 16:47:03 -0300

    Smaller indentation in .cpp files

--- content/docs/use/kxmlgui/hello_world/main1.cpp
+++ content/docs/use/kxmlgui/hello_world/main1.cpp
@@ -5,10 +5,12 @@ int main (int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    KGuiItem yesButton("Hello", QString(),
-                        "This is a tooltip",
-                        "This is a WhatsThis help text.");
+    KGuiItem yesButton(// Content text, Icon, Tooltip Text, WhatsThis text
+        "Hello", QString(),
+        "This is a tooltip",
+        "This is a WhatsThis help text.");
 
+    // If user clicks Yes button, return EXIT_SUCCESS, else return EXIT_FAILURE
     return KMessageBox::questionYesNo(0, "Hello World", "Hello", yesButton)
         == KMessageBox::Yes? EXIT_SUCCESS: EXIT_FAILURE;
 }
