ee0a9052e2361e304d430a7f72f5fd97f5fb051c
Thiago Sueto <herzenschein@gmail.com>
2025-06-27 19:21:45 -0300

    Add initial and final example code for Rust tutorial

---
+++ content/docs/getting-started/rust/rust-app/simplemdviewer/build.rs
@@ -0,0 +1,12 @@
+use cxx_qt_build::{CxxQtBuilder, QmlModule};
+
+fn main() {
+    CxxQtBuilder::new()
+        .qml_module(QmlModule {
+            uri: "org.kde.simplemdviewer",
+            qml_files: &["src/qml/Main.qml"],
+            rust_files: &["src/main.rs"],
+            ..Default::default()
+        })
+        .build();
+}
---
+++ content/docs/getting-started/rust/rust-app/simplemdviewer_final/README.md
@@ -0,0 +1,3 @@
+# Simple Markdown Viewer in Rust and Kirigami 🦀
+
+A simple Markdown viewer created with Kirigami, QML and Rust.
