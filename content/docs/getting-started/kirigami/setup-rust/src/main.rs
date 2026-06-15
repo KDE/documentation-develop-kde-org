be99c4362d4064a6c4d54d0cd07cc5f1c75b6ee0
Kaden Evens <kaden.evens@proton.me>
2026-06-15 19:54:01 +0000

    Update Rust tutorial documentation
    This PR updates the [Rust Tutorial](https://develop.kde.org/docs/getting-started/kirigami/setup-rust/) to:
    
    * Include up-to-date crate versions (as well as where to find the latest version)
    * The most recent Rust edition
    * Updated API usage in the build.rs script.
    * Better code readability in main.rs

--- content/docs/getting-started/kirigami/setup-rust/src/main.rs
+++ content/docs/getting-started/kirigami/setup-rust/src/main.rs
@@ -1,15 +1,4 @@
-#[cxx_qt::bridge]
-mod ffi {
-    extern "RustQt" {
-        #[qobject]
-        type DummyQObject = super::DummyRustStruct;
-    }
-}
-
-#[derive(Default)]
-pub struct DummyRustStruct;
-
-use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QQuickStyle, QString, QUrl};
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QQuickStyle};
 use cxx_qt_lib_extras::QApplication;
 use std::env;
 
@@ -18,15 +7,15 @@ fn main() {
     let mut engine = QQmlApplicationEngine::new();
 
     // To associate the executable to the installed desktop file
-    QGuiApplication::set_desktop_file_name(&QString::from("org.kde.kirigami_rust"));
+    QGuiApplication::set_desktop_file_name(&"org.kde.kirigami_rust".into());
     // To ensure the style is set correctly
     let style = env::var("QT_QUICK_CONTROLS_STYLE");
     if style.is_err() {
-        QQuickStyle::set_style(&QString::from("org.kde.desktop"));
+        QQuickStyle::set_style(&"org.kde.desktop".into());
     }
 
     if let Some(engine) = engine.as_mut() {
-        engine.load(&QUrl::from("qrc:/qt/qml/org/kde/tutorial/src/qml/Main.qml"));
+        engine.load(&"qrc:/qt/qml/org/kde/tutorial/src/qml/Main.qml".into());
     }
 
     if let Some(app) = app.as_mut() {
