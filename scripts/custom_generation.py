5fe492d8bb916b008677f059adeeb7c2e0db71e4
Thiago Sueto <herzenschein@gmail.com>
2026-06-15 15:55:43 -0300

    Install hugo-i18n with --break-system-packages
    Using the same reasoning as https://invent.kde.org/sysadmin/ci-images/-/commit/dc7a7b13a7582c5e4dbb5b9598a82e114c7c46d6?file_path=staticweb%2Fbuild.sh#line_51853cc12_A131

--- scripts/custom_generation.py
+++ scripts/custom_generation.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 os.environ["PACKAGE"] = 'documentation-develop-kde-org'
-os.system('git clone https://invent.kde.org/websites/hugo-i18n && pip3 install ./hugo-i18n')
+os.system('git clone https://invent.kde.org/websites/hugo-i18n && pip3 install --break-system-packages ./hugo-i18n')
 os.system('hugoi18n compile po')  # compile translations in folder "po"
 os.system('hugoi18n generate')
 
